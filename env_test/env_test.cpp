91a02d58e81f58279857f811ea7e36003cb42ddc
YC-Vertex <vertexprivate@gmail.com>
2021-09-14 22:57:36 +0800

    change compiler opt arg to -O0 to discover real mem ref stats

--- env_test/env_test.cpp
+++ env_test/env_test.cpp
@@ -17,24 +17,27 @@ int main() {
 #endif // VTUNE_ANALYSIS
 
     int *matA[N];
-    int *matB_T[N];
+    int *matB[N];
     int *matX[N];
+    int *matY[N];
 
     /* initialize matrix */
     cout << "Initializing data ..." << endl;
 
     for (int i = 0; i < N; ++i) {
         matA[i] = (int*) malloc(N * sizeof(int));
-        matB_T[i] = (int*) malloc(N * sizeof(int));
+        matB[i] = (int*) malloc(N * sizeof(int));
         for (int j = 0; j < N; ++j) {
             matA[i][j] = i * N + j;
-            matB_T[i][j] = i * N + j;
+            matB[i][j] = i * N + j;
         }
     }
 
     for (int i = 0; i < N; ++i) {
         matX[i] = (int*) malloc(N * sizeof(int));
+        matY[i] = (int*) malloc(N * sizeof(int));
         memset(matX[i], 0, N);
+        memset(matY[i], 0, N);
     }
 
 
@@ -47,7 +50,7 @@ int main() {
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
             for (int k = 0; k < N; ++k) {
-                matX[i][j] += matA[i][k] * matB_T[j][k];
+                matX[i][j] += matA[i][k] * matB[j][k];
             }
         }
     }
@@ -62,10 +65,10 @@ int main() {
 #if (defined VTUNE_ANALYSIS && defined MM_SLOW)
     __itt_resume();
 #endif // VTUNE_ANALYSIS
-    for (int k = 0; k < N; ++k) {
-        for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                matX[i][j] += matA[i][k] * matB_T[j][k];
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            for (int k = 0; k < N; ++k) {
+                matY[i][j] += matA[k][i] * matB[k][j];
             }
         }
     }
